c4e279d72f4640f901964c4149bfc4d9de9ec86c
Ali <alisalehi498@yahoo.com>
2023-02-08 07:09:35 +0330

    get messages that are coming from target

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
-use std::io::{BufRead, Write};
-use grammers_client::{Client, Config, InitParams, SignInError};
+use grammers_client::{Client, Config, InitParams, SignInError, Update};
+use grammers_client::types::Chat::Channel;
 use grammers_session::{Session};
 use serde_json;
 use crate::config::AppConfig;
-use tokio::{runtime};
-use crate::utils::{config_exists, is_valid};
+use tokio::{runtime, task};
+use crate::utils::{config_exists, is_valid, prompt};
 
 mod config;
 mod utils;
 
 const SESSION_FILE: &str = "scrapper.session";
+
 type Result = std::result::Result<(), Box<dyn std::error::Error>>;
-async fn main_async()->Result {
+
+async fn main_async() -> Result {
     if !config_exists()
     {
         println!("Config file not found");
@@ -35,48 +37,61 @@ async fn main_async()->Result {
         },
         session: Session::load_file_or_create(SESSION_FILE).expect("Failed to create session"),
     }).await;
-    if login.is_err(){
+    if login.is_err() {
         panic!("failed to connect to the telegram");
     }
-    let client = login.expect("failed to create client");
+    let client_handler = login.expect("failed to create client");
 
-    if !client.is_authorized().await.unwrap(){
+    if !client_handler.is_authorized().await.unwrap() {
         println!("you are not authorized,requesting verification code");
-        let login_token =client
-            .request_login_code(&config.phone,config.api_id,&config.api_hash).await
+        let login_token = client_handler
+            .request_login_code(&config.phone, config.api_id, &config.api_hash).await
             .expect("failed to send code");
         let code = prompt("Enter Code:").expect("failed to get the code");
 
-        let signed_in = client.sign_in(&login_token,&code).await;
+        let signed_in = client_handler.sign_in(&login_token, &code).await;
 
         match signed_in {
             Err(SignInError::PasswordRequired(password_token)) => {
-
                 let hint = password_token.hint().unwrap_or("None");
                 let prompt_message = format!("Enter the password (hint {}): ", &hint);
                 let password = prompt(prompt_message.as_str()).expect("failed to get the password");
-                client.check_password(password_token, password.trim()).await?;
+                client_handler.check_password(password_token, password.trim()).await?;
             }
             Ok(user) => {
-                println!("logged in with user:{},id:{}",user.username().unwrap(),user.id());
-            },
+                println!("logged in with user:{},id:{}", user.username().unwrap(), user.id());
+            }
             Err(e) => panic!("{}", e),
         };
-        match client.session().save_to_file(SESSION_FILE) {
-            Ok(_) => {println!("session saved to: {}", SESSION_FILE)},
+        match client_handler.session().save_to_file(SESSION_FILE) {
+            Ok(_) => { println!("session saved to: {}", SESSION_FILE) }
             Err(e) => {
-                println!("NOTE: failed to save the session[{}],you will sign out when program stops working",e);
+                println!("NOTE: failed to save the session[{}],you will sign out when program stops working", e);
             }
         }
-
     }
-    else {
-        println!("you are logged in");
+    println!("signed in");
+    let client = client_handler.clone();
+    let network = task::spawn(async move { client_handler.run_until_disconnected().await });
 
+    while let Some(update) = client.next_update().await? {
+        match update {
+            Update::NewMessage(message) if !message.outgoing() => {
+                match message.chat() {
+                    Channel(ch) if ch.username().unwrap().to_string() == config.from => {
+                        println!("{:#?}", message);
+                    }
+                    _ => {}
+                }
+            }
+            _ => {}
+        }
     }
 
+    network.await??;
     Ok(())
 }
+
 fn main() -> Result {
     runtime::Builder::new_current_thread()
         .enable_all()
@@ -85,16 +100,3 @@ fn main() -> Result {
         .block_on(main_async())
 }
 
-fn prompt(message: &str) -> Option<String> {
-    let stdout = std::io::stdout();
-    let mut stdout = stdout.lock();
-    stdout.write_all(message.as_bytes()).unwrap();
-    stdout.flush().unwrap();
-
-    let stdin = std::io::stdin();
-    let mut stdin = stdin.lock();
-
-    let mut line = String::new();
-    stdin.read_line(&mut line).unwrap();
-    return Some(line);
-}
--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,4 @@
+use std::io::{BufRead, Write};
 use crate::config::AppConfig;
 
 pub fn config_exists() -> bool {
@@ -13,4 +14,17 @@ pub fn is_valid(config: &AppConfig) -> bool {
         return false;
     }
     true
+}
+pub fn prompt(message: &str) -> Option<String> {
+    let stdout = std::io::stdout();
+    let mut stdout = stdout.lock();
+    stdout.write_all(message.as_bytes()).unwrap();
+    stdout.flush().unwrap();
+
+    let stdin = std::io::stdin();
+    let mut stdin = stdin.lock();
+
+    let mut line = String::new();
+    stdin.read_line(&mut line).unwrap();
+    return Some(line);
 }
