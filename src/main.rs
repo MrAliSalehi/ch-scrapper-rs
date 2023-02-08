9068f9514cd493aee236c272ac14faad1034f327
Ali <36738510+MrAliSalehi@users.noreply.github.com>
2023-02-08 16:47:34 +0330

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -75,7 +75,7 @@ async fn main_async() -> AsyncResult {
     Ok(())
 }
 
-async fn (conf: &AppConfig,  client: Client) -> AsyncResult {
+async fn handle_updates_async(conf: &AppConfig,  client: Client) -> AsyncResult {
     let image_dir = current_dir()?.join("images");
     let to = client.resolve_username(conf.to).await.expect("couldn't resolve the username").unwrap();
     while let Some(update) = client.next_update().await? {
